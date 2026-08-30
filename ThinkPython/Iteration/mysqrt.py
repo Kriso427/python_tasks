9c260b4539a9040dbe6745b50b41a0d56e42f932
kriso427 <kriso427>
2026-08-30 19:33:48 +0100

    fix mysqrt.py

--- ThinkPython/Iteration/mysqrt.py
+++ ThinkPython/Iteration/mysqrt.py
@@ -11,5 +11,5 @@ def mysqrt(a):
 
 print(f"{"a":<5} {"mysqrt(i)":<25} {"math.sqrt(i)":<25} {"diff":<25}")
 print(f"{"-":<5} {"---------":<25} {"------------":<25} {"----":<25}")
-for i in range(1, 10):
-    print(f"{i:<5} {mysqrt(i):<25} {math.sqrt(i):<25} {abs(mysqrt(i)-math.sqrt(i)):<25}")
+for (i) in (range(1, 10)):
+    print(f"{float(i):<5} {mysqrt(float(i)):<25} {math.sqrt(float(i)):<25} {abs(mysqrt(i)-math.sqrt(float(i))):<25}")
